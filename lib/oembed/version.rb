2c15aab241e622d55f465ca47fb8dabe178c97fc
Michael Andrews <michael@socialcast.com>
2012-11-12 18:23:57 -0800

    Bump version

--- lib/oembed/version.rb
+++ lib/oembed/version.rb
@@ -2,8 +2,9 @@ module OEmbed
   class Version
     MAJOR = 0
     MINOR = 8
-    PATCH = 7
-    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
+    PATCH = 8
+    LOCAL = 'sc1'
+    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}.#{LOCAL}"
     
     class << self
       # A String representing the current version of the OEmbed gem.
